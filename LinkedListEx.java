e221a709daa3ca15b0d957c341f4ddbdb82c738f
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-10-27 05:11:48 +0530

    Add files via upload

---
+++ LinkedListEx.java
@@ -0,0 +1,22 @@
+package com.listInterface;
+
+import java.util.LinkedList;
+
+public class LinkedListEx {
+	
+	public static void main(String[] args) {
+		
+		LinkedList l1 = new LinkedList();
+	
+		l1.add(100);
+		l1.add('c');
+		l1.add(true);
+		l1.add(2.5);
+		l1.add("hi");
+		l1.add(null);
+		
+		System.out.println(l1);
+	}
+
+
+}
