0038c082822bfaee1624d9e06a627bf7446686f0
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-11-13 03:03:02 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,21 @@
+package com.practice;
+
+public class Exception {
+	
+	public static void main(String s[]) {
+
+		try {
+
+		int x = 6/0;
+
+		}
+
+		catch(ArithmeticException e1) {
+
+		System.out.println("Arithmetic Exception is thrown");
+
+		}
+
+		}
+
+}
