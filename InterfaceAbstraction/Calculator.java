bb51eeff81d4d7239f8243b272c678df2ba5edb9
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-05 03:37:12 +0530

    Add files via upload

---
+++ InterfaceAbstraction/Calculator.java
@@ -0,0 +1,10 @@
+package com.interfaceAbs;
+
+public interface Calculator {
+	
+	void add(double a,double b);
+	void subtract(double a,double b);
+	void multiply(double a,double b);
+	void division(double a,double b);
+
+}
