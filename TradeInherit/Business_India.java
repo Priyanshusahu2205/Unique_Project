a404ac8d16a93400e0b7b5ec0260dac522ccc44f
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-06 04:33:29 +0530

    Add files via upload

---
+++ TradeInherit/Business_India.java
@@ -0,0 +1,9 @@
+package com.trade;
+
+public class Business_India {
+	public static void Rules() {
+		System.out.println("There are certain rules and regulation has to be follow to run business in India.");
+		
+	}
+
+}
