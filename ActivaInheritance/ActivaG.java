1e2785a2a7a2da2790f9164449904dbab6a3787e
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-06 04:39:15 +0530

    Add files via upload

---
+++ ActivaInheritance/ActivaG.java
@@ -0,0 +1,15 @@
+package com.inheritance1;
+
+public class ActivaG extends Activa {
+	
+	//constructor
+	ActivaG(){
+		System.out.println("I have a Smart and Latest Features");
+		
+	}
+	 public static void speed() {
+		 System.out.println("120");
+		 
+	 }
+
+}
