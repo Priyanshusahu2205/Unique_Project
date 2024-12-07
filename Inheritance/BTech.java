f5a626b19884dc627cc62655602dc4e9c3a052d3
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-08 03:07:31 +0530

    Add files via upload

---
+++ Inheritance/BTech.java
@@ -0,0 +1,20 @@
+package com.inheritance;
+
+public class BTech {
+	
+	String dept;
+	int id;
+		
+	public BTech() {
+		System.out.println("BCS College");
+			
+	}
+		
+	public void displayCollege(String dept, int id) {
+		System.out.println("Department is:-"+ dept +","+ "Id is:-"+id);
+			
+			
+	}
+
+}
+
