8263c263b4ed41ee8250d5842ff69ebc69e52f53
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-07 20:17:19 +0530

    Add files via upload

---
+++ MethodOverriding/MainP.java
@@ -0,0 +1,12 @@
+package com.inheritance1;
+
+public class MainP {
+	
+	public static void main(String[] args) {
+		ChildMethodOverriding c1 = new ChildMethodOverriding();
+		
+		c1.print_info("Raaj");
+		c1.print_info1("Priya");
+	}
+
+}
