21219053266a1ff8bf84862e20e3c3ea689dbc47
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-07 20:16:38 +0530

    Add files via upload

---
+++ MethodOverriding/ChildMethodOverriding.java
@@ -0,0 +1,13 @@
+package com.inheritance1;
+
+public class ChildMethodOverriding extends ParentMethodOverriding {
+	
+	String name="Priya";
+	
+	public void print_info1(String name) {
+		System.out.println("Child name:-" +name);
+		
+	
+	}
+
+}
