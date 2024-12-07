ba1ecf9fec933e0409f75179cd4e1c54f8199369
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-07 20:16:23 +0530

    Add files via upload

---
+++ MethodOverriding/ParentMethodOverriding.java
@@ -0,0 +1,15 @@
+package com.inheritance1;
+
+public class ParentMethodOverriding {
+	
+	String name="Raaj";
+	
+	
+	//Method
+	public void print_info(String name) {
+		
+		System.out.println("Name of Parent:-"+name);
+		
+	}
+
+}
