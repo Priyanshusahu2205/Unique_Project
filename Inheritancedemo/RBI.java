c01e00d1b76dd6ab1ce07abeb9a006f72d9f6e82
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-09 04:55:03 +0530

    Add files via upload

---
+++ Inheritancedemo/RBI.java
@@ -0,0 +1,16 @@
+package com.inheritance2;
+
+public class RBI {
+	public void rbi_loan_dept() {
+		System.out.println("Rules are released for this financial year.");
+		
+	}
+	
+	public void rate_interest() {
+		System.out.println("Rate of interest for this year.");
+		
+	}
+
+}
+
+
