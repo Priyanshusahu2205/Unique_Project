13961782075b2c344119ce63d33e0c68e7191f99
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-10 20:00:07 +0530

    Add files via upload

---
+++ InheritanceM/Main.java
@@ -0,0 +1,16 @@
+package com.inheritanceM;
+
+public class Main {
+	public static void main(String[] args) {
+			
+			Student1 s1 = new Student1();
+			
+			s1.print_info("Raaj",22,"Bhilai");
+			s1.print_info1("Priyanshu","Bhilai",1200000);
+			s1.print_info2("Shipra","CSE",120);
+			s1.print_info2("Priya","CSE",122);
+			
+		
+	}
+
+}
