0fc33747ac98275626361f3b7baceeff4d02e1f7
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-11 04:29:02 +0530

    Add files via upload

---
+++ InheritanceEx/MainPC.java
@@ -0,0 +1,13 @@
+package com.inheritance3;
+
+public class MainPC {
+	public static void main(String[] args) {
+		Child c1 = new Child("Raaj","p22","Bhilai");
+		
+		c1.printInfo();
+		c1.parent_method();
+		c1.m1();
+		
+	}
+
+}
