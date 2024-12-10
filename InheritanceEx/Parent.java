479087786388c7e3ec212d5585ec79800378cb8b
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-11 04:28:36 +0530

    Add files via upload

---
+++ InheritanceEx/Parent.java
@@ -0,0 +1,24 @@
+package com.inheritance3;
+
+public class Parent {
+	String name="Raaj";
+	String id="p22";
+	
+	public Parent(String name, String id) {
+		this.name=name;
+		this.id=id;
+	}
+	
+	//Method
+	public void parent_method() {
+		System.out.println("Parent method");
+		
+	}
+	
+	public void m1() {
+		System.out.println("m1 ...");
+		
+	}
+
+}
+
