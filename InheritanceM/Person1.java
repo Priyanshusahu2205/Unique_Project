c87be73e5835778a0ffcf74ae51575d2899bf8cf
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-10 19:59:13 +0530

    Add files via upload

---
+++ InheritanceM/Person1.java
@@ -0,0 +1,17 @@
+package com.inheritanceM;
+
+public class Person1 {
+	String name;
+	int id;
+	String city;
+	
+	
+	public void print_info(String name, int id, String city) {
+		System.out.println("Name:-"+name);
+		System.out.println("Id:-"+id);
+		System.out.println("City:-"+city);
+
+	}
+
+}
+
