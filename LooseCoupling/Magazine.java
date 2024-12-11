2bccc025b43201d5956297f51082ad431911f723
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-12 04:20:04 +0530

    Add files via upload

---
+++ LooseCoupling/Magazine.java
@@ -0,0 +1,19 @@
+package com.looseCoupling;
+
+public class Magazine implements Books {
+
+	@Override
+	public void read() {
+		// TODO Auto-generated method stub
+		System.out.println("There are many magazine to read.");
+		
+	}
+
+	@Override
+	public void write() {
+		// TODO Auto-generated method stub
+		System.out.println("There are many magazine to write.");
+		
+	}
+
+}
