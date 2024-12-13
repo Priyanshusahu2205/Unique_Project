a67d268f9283a9462b899c1f09ebf19ca7ea8061
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-12-14 03:17:15 +0530

    Add files via upload

---
+++ Static.final/Final.java
@@ -0,0 +1,14 @@
+package com.static1;
+
+public class Final {
+	final int speed=120;
+	
+	void run() {
+		int speed=100;
+	}
+	//method
+	void show() {
+		System.out.println("Running on a speed of "+speed+".");
+	}
+
+}
